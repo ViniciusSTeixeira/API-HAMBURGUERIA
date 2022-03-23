f034d09f7367291a0a0930ba8b94aa49ed3ea2c9
Vinicius Teixeira <93617202+ViniciusSTeixeira@users.noreply.github.com>
2022-03-23 15:44:04 -0300

    Update dotenv

--- app.js
+++ app.js
@@ -8,11 +8,13 @@ const port = process.env.PORT || 3003;
 
 const sequelize = require("./src/database/conectar.js"); /*conexão com o banco*/
 
-const models = require('./src/models/EnderecoModels.js');/* conexão com models */
+const models = require("./src/models/EnderecoModels.js"); /* conexão com models */
 
-const enderecoRouter = require('./src/routers/enderecoRotas.js'); /* conexão com as rotas */
+const enderecoRouter = require("./src/routers/enderecoRotas.js"); /* conexão com as rotas */
 
-app.use('/endereco', enderecoRouter); /* ROTA */
+app.use("/endereco", enderecoRouter); /* ROTA */
+
+
 
 app.get("/teste", (req, res) => {
   return res.status(200).json({ menssagem: "servidor funcionando" });
